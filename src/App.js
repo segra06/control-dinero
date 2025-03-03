4566a779a57b53978989d69aaab1ef505ef39333
Segrita <segra06@hotmail.com>
2025-03-02 19:26:10 -0600

    Agrega antD, tiene un login sencillo y falta revisar el message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Checkbox, Form, Input, message, Typography } from 'antd'; // Importa Typography
+import { useNavigate } from 'react-router-dom'; // Cambiado a useNavigate
 
-function App() {
+const validUsername = 'sebastian';  // Credenciales predefinidas
+const validPassword = '12345';
+
+const App = () => {
+  const navigate = useNavigate();  // Usamos useNavigate en lugar de useHistory
+
+  const onFinish = (values) => {
+    if (values.username === validUsername && values.password === validPassword) {
+      message.success('¡Bienvenido!');
+      navigate('/dashboard');  // Redirigir al Dashboard
+    } else {
+      message.error('Usuario o contraseña incorrectos');
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Typography.Title level={2} style={{ textAlign: 'center' }}>Control de Dinero</Typography.Title> {/* Agrega el título */}
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor ingrese su nombre de usuario!',
+            },
+          ]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor ingrese su contraseña!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" label={null}>
+          <Checkbox>Recordarme</Checkbox>
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            Iniciar sesión
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
-}
+};
 
 export default App;
