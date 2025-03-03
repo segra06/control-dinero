3c46b698a80af8f6e8b18c17407255c9cfc022cc
Segrita <segra06@hotmail.com>
2025-03-02 20:20:09 -0600

    Se mejoró el login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Button, Checkbox, Form, Input, message, Typography } from 'antd'; // Importa Typography
-import { useNavigate } from 'react-router-dom'; // Cambiado a useNavigate
+import React, { useState } from 'react';
+import { Button, Checkbox, Form, Input, message, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
-const validUsername = 'sebastian';  // Credenciales predefinidas
+const validUsername = 'sebastian';
 const validPassword = '12345';
 
 const App = () => {
-  const navigate = useNavigate();  // Usamos useNavigate en lugar de useHistory
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const [errorMessage, setErrorMessage] = useState('');
 
   const onFinish = (values) => {
     if (values.username === validUsername && values.password === validPassword) {
       message.success('¡Bienvenido!');
-      navigate('/dashboard');  // Redirigir al Dashboard
+      navigate('/dashboard');
     } else {
-      message.error('Usuario o contraseña incorrectos');
+      setErrorMessage('Que menso');
     }
   };
 
@@ -23,53 +25,62 @@ const App = () => {
 
   return (
     <>
-      <Typography.Title level={2} style={{ textAlign: 'center' }}>Control de Dinero</Typography.Title> {/* Agrega el título */}
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Por favor ingrese su nombre de usuario!',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+      <Typography.Title level={2} style={{ textAlign: 'center' }}>Finanzas</Typography.Title>
+      <div style={{ display: 'flex', justifyContent: 'center' }}> {/* Contenedor para centrar */}
+        <div style={{ marginLeft: '0px', marginTop: '50px' }}> {/* Ajusta el valor de marginLeft según sea necesario */}
+          <Form
+            form={form}
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: '¡Por favor ingrese su nombre de usuario!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Por favor ingrese su contraseña!',
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: '¡Por favor ingrese su contraseña!',
+                },
+              ]}
+            >
+              <Input.Password/>
+            </Form.Item>
 
-        <Form.Item name="remember" valuePropName="checked" label={null}>
-          <Checkbox>Recordarme</Checkbox>
-        </Form.Item>
-
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
-            Iniciar sesión
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item name="remember" valuePropName="checked" label={null}>
+              <Checkbox>Recordarme</Checkbox>
+            </Form.Item>
+          </Form>
+          {errorMessage && (
+            <div style={{ color: 'red', textAlign: 'center', marginTop: '10px' }}>
+              {errorMessage}
+            </div>
+          )}
+          <div style={{ marginTop: '10px', textAlign: 'center' }}> {/* Ajusta el valor de marginTop según sea necesario */}
+            <Button type="primary" style={{ width: '100%' }} onClick={() => form.submit()}>
+              Iniciar sesión
+            </Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
